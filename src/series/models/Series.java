31b494963ca8c0098851fdb32ab1d47ea3907f22
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-11-13 12:57:23 -0300

    Se hicieron los getter y setters, se hizo la estructura basica de la clase Series

--- src/series/models/Series.java
+++ src/series/models/Series.java
@@ -5,10 +5,102 @@
  */
 package series.models;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Diego
  */
 public class Series {
+    private int idSeries;
+    private String title;
+    private String detail;
+    private LocalDate released;
+    private int rate;
+    private String genre;
+    private float price;
+    private boolean ATP;
+    private String state;
+
+    public Series(int idSeries, String title, String detail, LocalDate released, int rate, String genre, float price, boolean ATP) {
+        this.idSeries = idSeries;
+        this.title = title;
+        this.detail = detail;
+        this.released = released;
+        this.rate = rate;
+        this.genre = genre;
+        this.price = price;
+        this.ATP = ATP;
+        this.state = "AC";
+    }
+
+    public int getIdSeries() {
+        return idSeries;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public LocalDate getReleased() {
+        return released;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public boolean isATP() {
+        return ATP;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public void setReleased(LocalDate released) {
+        this.released = released;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setATP(boolean ATP) {
+        this.ATP = ATP;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+    
     
 }
